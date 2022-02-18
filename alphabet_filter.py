8545a423cd64fef9e407b6cbaed6a0c51f33a5c7
vandana <vandanaprathapar@gmail.com>
2022-02-19 09:20:28 +1100

    incorporates the functions as methods of the class LetterFilter, need to use constraints later

--- alphabet_filter.py
+++ alphabet_filter.py
@@ -1,15 +1,26 @@
-VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+"""
+ALPHABET_FILTER.PY 
 
-def filter_vowels(s):
-    return ''.join([i for i in s if i not in VOWELS])
-    
-    
-def filter_consonants(s): 
-        return ''.join([i for i in s if i in VOWELS])
+For a given definition of a class LetterFilter, complete its methods filter_vowels and filter_consonants. The class takes a string in the constructor and stores it to its s attribute. 
+The method filter_vowels must return a new string with all vowels removed from it. Similarly, the method filter_consonants must return a new string with all consonants removed from it.
+
+"""
+
+class LetterFilter(): 
+    def __init__(self, s):
+        self.string = s.lower()
+        self.vowels = ['a', 'e', 'i', 'o', 'u']
+
+    def filter_vowels(self):
+        return ''.join([i for i in self.string if i not in self.vowels]) 
+        
+        
+    def filter_consonants(self): 
+        return ''.join([i for i in self.string if i in self.vowels])
 
 if __name__ == "__main__":
-    string_f = input("Enter String: ")
+    string_f = LetterFilter(input("Enter String: "))
     
-    print(f'{filter_vowels(string_f)}, {filter_consonants(string_f)}')
+    print(f'{string_f.filter_vowels()}, {string_f.filter_consonants()}')
 
 
